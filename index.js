8b66fea1182ccdcf1b36a39b3411b4a4cfb9d170
springleo <782655835@qq.com>
2018-12-19 14:23:30 +0800

    deploy

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+const path = require('path')
 const express = require('express')
+const bodyParse = require('body-parser')
+const router = require('./server/router')
 const app = express()
 
-app.use(express.static(`${__dirname}/dist`))
+const resolve = file => path.resolve(__dirname, file)
+app.use(express.static(resolve('./dist')))
+app.use(bodyParse.json())
+app.use(bodyParse.urlencoded({ extended: true }))
+app.use('/api', router)
+
 app.listen(3000, () => console.log('http://localhost:3000 successed'))
