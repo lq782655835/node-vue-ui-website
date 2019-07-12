cae899a819af27cec00a64f70d7b07ef6bb08956
springleo <782655835@qq.com>
2019-07-12 17:12:45 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,7 @@ echo 'local commited'
 # 发布到 github
 git push -f https://github.com/lq782655835/node-vue-ui-website.git master
 echo 'remote commited'
-# cd -
+
+git push origin `git subtree split --prefix dist master`:gh-pages --force
+# git subtree push --prefix site origin gh-pages
+echo 'build in gh-pages'
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, './dist'),
-        publicPath: '/',
+        publicPath: '/node-vue-ui-website/',
         filename: '[name].js'
     },
     module: {
